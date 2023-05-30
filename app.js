aea462b250c5679897238816c41fcb825cd6ecb6
kalrog-dev <michal7272@seznam.cz>
2023-05-30 18:33:38 +0200

    add form validation test

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
+// Form variables
+const emailInput = document.getElementById('email-input');
 const submitBtn = document.getElementById('newsletter-submit-btn');
+const dismissBtn = document.getElementById('success-dismiss-btn');
+const formGroup = document.querySelector('.form-group');
+
+// Redirect to Success page on-submit
 submitBtn.addEventListener('click', (e) => {
+    // Prevent form submission
     e.preventDefault();
-    window.location.href = './success.html';
+
+    // Email validation
+    if (emailCheck(emailInput.value)) {
+        // If email passes the validation test, remove the error class and redirect to Seccess page
+        formGroup.classList.remove('error');
+        window.location.href = './success.html';
+    } else {
+        // If email fails the test, add an error class
+        formGroup.classList.add('error');
+    }
 });
+
+// Email validation test
+function emailCheck(str) {
+    const regex = /^[\w]+@[\w]+[.]{1}[\w]+$/;
+    return regex.test(str);
+}
